e9965446a4328bb8edd58eb063f3abbfe23c220a
PC <PC@DESKTOP-68E36JR>
2026-05-24 22:32:37 +0200

    added repating if exam is already full capacity

--- ExamAutomaticSignUp/Program.cs
+++ ExamAutomaticSignUp/Program.cs
@@ -1,24 +1,119 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Collections.ObjectModel;
 using System.Globalization;
 
 
 const string configFilePath = "../../../config.txt";
 
 string username, password, url, dateStr;
+int delayTimeInMinutes;
 
-GetInput(out username, out password, out url);
+GetInput(out username, out password, out url, out delayTimeInMinutes);
 
 IWebDriver driver = new ChromeDriver();
 driver.Navigate().GoToUrl(url);
 
-if (NeedToLogIn(driver))
+if (NeedToLogIn())
 {
-    await LogIn(driver, username, password);
+    await LogIn();
+    Console.WriteLine($"{DateTime.Now.Hour}:{DateTime.Now.Minute} - Successfully logged in.");
     driver.Navigate().GoToUrl(url);
 }
 
-void GetInput(out string username, out string password, out string url)
+await Task.Delay(1000);
+
+while (true)
+{
+    IWebElement table = driver.FindElement(By.CssSelector("table.SRH-inp2"));
+
+    ClickCookieBanner();
+
+    var rows = table.FindElements(By.TagName("tr"));
+    foreach (var row in rows)
+    {
+        var cells = row.FindElements(By.TagName("td"));
+
+        if (cells.Count == 0)
+            continue;
+
+        string rowDate = cells[0].Text.Trim();
+
+        if (rowDate == dateStr)
+        {
+            int capacity = int.Parse(cells[3].Text);
+            int alreadyRegistered = int.Parse(cells[4].Text);
+            if (alreadyRegistered == capacity)
+            {
+                Console.WriteLine($"{DateTime.Now.Hour}:{DateTime.Now.Minute} - No available slots for the selected date.");
+                break;
+            }
+
+            ClickRegisterButton(cells);
+
+            return;
+        }
+    }
+    
+    await Task.Delay(delayTimeInMinutes * 60 * 1000);
+}
+
+void ClickRegisterButton(IList<IWebElement> cells)
+{
+    try
+    {
+        IWebElement registerLink = cells.Last().FindElement(
+            By.XPath(".//a[img[@title='Prihlásenie na termín']]")
+        );
+
+        registerLink.Click();
+
+        Console.WriteLine($"{DateTime.Now.Hour}:{DateTime.Now.Minute} - Successfully clicked register button.");
+    }
+    catch (NoSuchElementException)
+    {
+        Console.WriteLine($"{DateTime.Now.Hour}:{DateTime.Now.Minute} - Register button not found.");
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"{DateTime.Now.Hour}:{DateTime.Now.Minute} - Failed to click register button: {ex.Message}");
+    }
+}
+
+void ClickCookieBanner()
+{
+    try
+    {
+        var cookiesButton = driver.FindElement(By.CssSelector("#hlaska-cookies button, #hlaska-cookies a"));
+        cookiesButton.Click();
+        Console.WriteLine($"{DateTime.Now.Hour}:{DateTime.Now.Minute} - Cookies banner closed.");
+    }
+    catch
+    {
+        return;
+    }
+}
+
+bool NeedToLogIn()
+{
+    var loginButton = driver.FindElements(By.Id("login"));
+    if (loginButton.Count > 0) return true;
+
+    return false;
+}
+
+async Task LogIn()
+{
+    var usernameInput = driver.FindElement(By.Id("meno"));
+    var passwordInput = driver.FindElement(By.Id("heslo"));
+    var loginButton = driver.FindElement(By.Id("login"));
+    usernameInput.SendKeys(username);
+    passwordInput.SendKeys(password);
+    loginButton.Click();
+    await Task.Delay(1000);
+}
+
+void GetInput(out string username, out string password, out string url, out int delayTimeInMinutes)
 {
     string[] fileLines;
     try
@@ -30,20 +125,34 @@ void GetInput(out string username, out string password, out string url)
         fileLines = new string[0];
     }
 
-    if (fileLines.Length != 4)
+    if (fileLines.Length != 5)
     {
         Console.WriteLine("Config file not found or invalid. Please enter the required information:");
         username = WaitForUsername();
         password = WaitForPassword();
         url = WaitForUrl();
         dateStr = WaitForDate();
+        delayTimeInMinutes = WaitForDelayTimeInMinutes();
     }
     else
     {
         url = fileLines[0].Trim(); // url
         username = fileLines[1].Trim(); // username
         password = fileLines[2].Trim(); // password
+        dateStr = fileLines[3].Trim(); // date
+        delayTimeInMinutes = int.Parse(fileLines[4].Trim()); // delay time in minutes
     }
+}
+
+int WaitForDelayTimeInMinutes()
+{
+    Console.WriteLine("Enter delay time in minutes:");
+    int delayTimeInMinutes;
+    while (!int.TryParse(Console.ReadLine(), out delayTimeInMinutes))
+    {
+        Console.WriteLine("Invalid input. Please enter a valid number of minutes:");
+    }
+    return delayTimeInMinutes;
 }
 
 string WaitForUsername()
@@ -82,7 +191,6 @@ string WaitForUrl()
     return url;
 }
 
-// did not check if it works, who cares, nobody will use this, not even me, just fun
 string WaitForDate()
 {
     Console.WriteLine("Enter date (dd.MM.yyyy / hh:mm):");
@@ -106,30 +214,11 @@ string WaitForDate()
     Calendar calendar = CultureInfo.CurrentCulture.Calendar;
     try
     {
-        calendar.ToDateTime(yearInt, monthInt, dayInt, hourInt, minuteInt, 0, 0); // this will throw an exception if the date is not valid (e.g. 30.02.2024)
+        calendar.ToDateTime(yearInt, monthInt, dayInt, hourInt, minuteInt, 0, 0);
     } catch { 
         Console.WriteLine("Invalid date. Please enter again.");
         return WaitForDate();
     }
 
     return dateStr;
-}
-
-bool NeedToLogIn(IWebDriver driver)
-{
-    var loginButton = driver.FindElements(By.Id("login"));
-    if (loginButton.Count > 0) return true;
-
-    return false;
-}
-
-async Task LogIn(IWebDriver driver, string username, string password)
-{
-    var usernameInput = driver.FindElement(By.Id("meno"));
-    var passwordInput = driver.FindElement(By.Id("heslo"));
-    var loginButton = driver.FindElement(By.Id("login"));
-    usernameInput.SendKeys(username);
-    passwordInput.SendKeys(password);
-    loginButton.Click();
-    await Task.Delay(1000);
 }
